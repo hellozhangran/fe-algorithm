634810681cfc80edb8150c229c607d20d11079be
zhangran22 <zhangran22@meituan.com>
2020-08-27 15:23:01 +0800

    b t

--- binary-tree.js
+++ binary-tree.js
@@ -2,6 +2,16 @@
  * 创建一个二叉树
  */
 
+
+/**
+ * 最开始自己想根据数组来创建一个二叉树，注意不是搜索二叉树！但无奈搞不定啊，先记录到这里吧
+ * https://juejin.im/post/6865556893637541902
+ */
+
+/**
+ * 创建搜索二叉树
+ */
+
 class Node {
     constructor(data, left, right) {
         this.data = data;
